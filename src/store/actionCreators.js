8b24902843541e20ef4b431cd3f26b567945c47e
Danyqueen <996802059@qq.com>
2020-01-22 14:32:36 +0800

    基于react的todolist-demo

---
+++ src/store/actionCreators.js
@@ -0,0 +1,16 @@
+// 所有action统一创建，增强代码可维护性
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM,  DELETE_TODO_ITEM} from './actionTypes'
+
+export const getInputChangeAction = (value) => ({
+  type: CHANGE_INPUT_VALUE,
+  value: value
+})
+
+export const addItemAction = () => ({
+  type: ADD_TODO_ITEM
+})
+
+export const deleteItemAction = (index) => ({
+  type: DELETE_TODO_ITEM,
+  index: index
+})
